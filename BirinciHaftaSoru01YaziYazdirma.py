1e6bccc4cec59db09781d3bc6cea0a467e7d8e7b
FikretYilmaz <51508851+FikretYilmaz@users.noreply.github.com>
2019-06-20 23:29:24 +0200

    Add files via upload

---
+++ BirinciHaftaSoru01YaziYazdirma.py
@@ -0,0 +1,16 @@
+veri = input("Lutfen Sevdiginiz Herhangi Bir Kisiyi veya Nesneyi Yaziniz")
+print("""
+				    ******************
+				  **                  **
+			    **						**
+			  **						  **
+		    **								**
+		  **             					  **""")
+print(" "*23,veri,"\n"+" "*23, "_"*len(veri),"""              
+		  **								  **
+		    **								**
+		      **						  **
+		        **						**
+		          **				  **
+		            ******************
+""")
---
+++ BirinciHaftaSoru04FaizHesaplama.py
@@ -0,0 +1,11 @@
+anaPara = float(input("Lutfen Faiz Hesaplamasi Istediginiz Ana Para Miktraini Giriniz"))
+yil = int(input("Lutfen Sureyi Yil Cinsinden Giriniz"))
+faizOrani = float(input("Lutfen Yillik Faiz Oranini Giriniz"))
+toplamFaiz = anaPara*yil*faizOrani/100
+aylikFaiz =toplamFaiz/12
+gunlukFaiz = toplamFaiz/365
+toplamPara = toplamFaiz + anaPara
+print("Toplam Faiz Tutari :",toplamFaiz,
+	"Aylik Faiz Tutari : ", aylikFaiz,
+	"Gunluk Ortalama Faiz Tutari :",gunlukFaiz,
+	"Toplam Para Tutari :",toplamPara,sep="\n")
